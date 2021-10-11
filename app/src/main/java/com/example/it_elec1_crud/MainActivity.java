8ab187e0269352d04c6d6ccb02336f4ef334a4b4
GRWolfy <67980159+GRWolfy@users.noreply.github.com>
2021-10-11 09:14:02 +0800

    w

--- app/src/main/java/com/example/it_elec1_crud/MainActivity.java
+++ app/src/main/java/com/example/it_elec1_crud/MainActivity.java
@@ -3,12 +3,36 @@ package com.example.it_elec1_crud;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.*;
+import android.view.*;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Data Bindings
+        btnAdd = findViewById(R.id.btnAdd);
+        btnUpdate = findViewById(R.id.btnUpdate);
+        btnDelete = findViewById(R.id.btnDelete);
+        editText_Firstname = findViewById(R.id.editText_FirstName);
+        editText_Middlename = findViewById(R.id.editText_MiddleName);
+        editText_Lastname = findViewById(R.id.editText_LastName);
+
+        //OnCLick Listener
+        btnAdd.setOnClickListener(this);
+        btnUpdate.setOnClickListener(this);
+        btnDelete.setOnClickListener(this);
+    }
+
+    //Declarations
+    EditText editText_Firstname, editText_Middlename, editText_Lastname;
+    Button btnAdd, btnUpdate, btnDelete;
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
